5a58e1c8151c109ce29561f210bd5b6a953ba714
George Pittas <fozuzip@gmail.com>
2023-06-14 19:03:26 +0300

    Add poppins font

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 function App() {
   return (
-    <div className="h-screen w-full bg-pattern bg-cover p-4">
-      <div>
-        <h1>Country Quiz</h1>
-        <div>
+    <div className="flex h-screen w-full flex-col content-center justify-center bg-pattern bg-cover p-4 font-poppins">
+      <div className="m-auto  w-full md:w-[464px]">
+        <h1 className="pb-2 text-4xl font-bold uppercase text-white">
+          Country Quiz
+        </h1>
+        <div className="bg-white p-8">
           <p>Kuala Lumpur is the capital of</p>
         </div>
       </div>
